b1ebb090eddaaea9a916118728fe87b5df1687dd
jvbsl <jvsbl@gmx.de>
2021-10-15 00:31:30 +0200

    import powershell get

--- LitGit.ps1
+++ LitGit.ps1
@@ -1,5 +1,5 @@
 #Get-Module -ListAvailable
-$m = Get-Module -ListAvailable OneGet
+$m = Get-Module -ListAvailable PowerShellGet
 Import-Module -ModuleInfo $m
 $m = Get-Module -ListAvailable PackageManagement
 Import-Module -ModuleInfo $m
