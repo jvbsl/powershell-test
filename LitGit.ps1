976e36d58aca64522d4da3e5f1f019b8a369d52f
jvbsl <jvsbl@gmx.de>
2021-10-15 00:45:54 +0200

    try importing desktop comp

--- LitGit.ps1
+++ LitGit.ps1
@@ -1,8 +1,8 @@
 Get-Module -ListAvailable -PSEdition Desktop
 
 
-$m = Get-Module -ListAvailable PowerShellGet
+$m = Get-Module -ListAvailable PowerShellGet -PSEdition Desktop
 Import-Module -ModuleInfo $m
-$m = Get-Module -ListAvailable PackageManagement
+$m = Get-Module -ListAvailable PackageManagement -PSEdition Desktop
 Import-Module -ModuleInfo $m
 Get-Package -Name Microsoft.Extensions.FileSystemGlobbing -ErrorAction SilentlyContinue
