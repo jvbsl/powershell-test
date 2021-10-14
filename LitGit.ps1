97a87981a8abf857a2c810e000aa384b3ac59439
jvbsl <jvsbl@gmx.de>
2021-10-15 00:53:39 +0200

    dynamic ps edition

--- LitGit.ps1
+++ LitGit.ps1
@@ -3,6 +3,6 @@
 
 #$m = Get-Module -ListAvailable PowerShellGet -PSEdition Desktop
 #Import-Module -ModuleInfo $m
-$m = Get-Module -ListAvailable PackageManagement -PSEdition Desktop
+$m = Get-Module -ListAvailable PackageManagement -PSEdition $PSEdition
 Import-Module -ModuleInfo $m
 Get-Package -Name Microsoft.Extensions.FileSystemGlobbing
